a13183d67f01a8dca9f388bbf37a2490eff52823
LINGESH036 <lingeshl497@gmail.com>
2026-06-12 10:53:58 +0530

    first com

---
+++ gitpro2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitpro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello kavi");
+            Console.WriteLine("welcome to trichy");
+        }
+    }
+}
